b60f1d220f56010f6de05640e98725334edcd3c2
ian <ian.aquino2003@gmail.com>
2020-07-10 11:06:58 -0300

    Android Bug Fix

--- src/pages/camera/camera.js
+++ src/pages/camera/camera.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Text, View, TouchableOpacity, StyleSheet,Alert } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet,Alert, Platform} from 'react-native';
 import { Camera } from 'expo-camera';
 
 import { useNavigation } from '@react-navigation/native';
@@ -11,7 +11,6 @@ import Constants from "expo-constants";
 import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
 
-
 export default function App() {
   const [imageUri, setImageUri] = useState(null);
   const [hasPermission, setHasPermission] = useState(null);
@@ -19,7 +18,7 @@ export default function App() {
   const [CameraRef, setCameraRef] = useState();
   
   const navigation = useNavigation();
-
+  
   async function imagePickerCall() {
     if (Constants.platform.ios) {
       const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
@@ -31,20 +30,20 @@ export default function App() {
     const data = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All
     });
-
+    
     if (data.cancelled) {
       return;
     }
-
+    
     if (!data.uri) {
       return;
     }
   }
-
-    function handleNavigateToHome(){
+  
+  function handleNavigateToHome(){
     navigation.navigate('Home');
-}
-
+  }
+  
 
   useEffect(() => {
     (async () => {
@@ -59,35 +58,53 @@ export default function App() {
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
+
   return (
     imageUri ?
     <ImageBackground style={styles.preview} source={{ uri: imageUri }}>
     </ImageBackground>
     :
     <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={type} ref={ref => setCameraRef(ref)}>
-          <TouchableOpacity style={{ 
+      <Camera style={styles.cameradiv} type={type} ref={ref => setCameraRef(ref)}>
+          <TouchableOpacity style={ Platform.OS === 'ios' ? { 
               flex:1,
               alignItems:"center",
               alignSelf:'flex',
               top:'3%',
-            }} 
-            onPress={handleNavigateToHome}>
+            }:
+            { 
+              flex:1,
+              alignItems:"center",
+              top:'3%',
+              right:'40%',
+            }
+            
+          } 
+          onPress={handleNavigateToHome}>
               <MaterialCommunityIcons name="arrow-left" size={45} color="white" />
           </TouchableOpacity>
+          </Camera>
         <View
           style={{
             flex: 1,
             alignItems: 'center',
-            backgroundColor: 'transparent',
+            backgroundColor: 'black',
             flexDirection: 'row',
           }}>
           <TouchableOpacity
-            style={{
+            style={ Platform.OS === 'ios' ? {
               flex: 0.2,
               alignSelf: 'flex-end',
               alignItems: 'center',
-            }}
+            }
+            :
+            {
+              flex: 0.2,
+              top:'40%',
+              alignItems: 'center',
+            }
+          
+          }
             onPress={() => {
               setType(
                 type === Camera.Constants.Type.back
@@ -97,32 +114,60 @@ export default function App() {
             }}>
               <MaterialCommunityIcons name="camera-retake" size={50} color="white" />
           </TouchableOpacity>
-          <TouchableOpacity style={{
+          <TouchableOpacity style={ Platform.OS === 'ios' ? {
             flex: 0.2,
             alignSelf: 'flex-end',
             alignItems: 'center',
             bottom:'-1%',
             left:'24%',         
-          }} onPress={async() => {
+          }
+        : {
+          flex: 0.2,
+          alignItems: 'center',
+          bottom:'-1%',
+          top:'40%',
+          left:'24%',         
+        }
+      } onPress={async() => {
             if(CameraRef){
               let photo = await CameraRef.takePictureAsync();
               console.log('photo', photo);
-            }
-          }}
+          //    setUpload(photo);
+          //     const path = photo.uri.split('/');
+          //     const name = path[path.length - 1];
+          //     console.log(name)
+          //     const data = new FormData();
+          //     data.append("photo", {
+          //       name: name,
+          //       uri: photo.uri,
+          //       });
+          //       console.log(photo.uri)
+          //     console.log(data)
+          //     await Axios.post("http://localhost:3333/upload", data);
+           }
+        }}
           >
             <MaterialCommunityIcons name="camera" size={50} color="white" />
           </TouchableOpacity>
-          <TouchableOpacity style={{
+          <TouchableOpacity style={ Platform.OS === 'ios' ? {
             flex: 0.2,
             alignSelf: 'flex-end',
             alignItems: 'center',
-            bottom:'-1%',
+            bottom:'0%',
             left:'44%',         
-          }} onPress={imagePickerCall}>
+          }
+          :
+          {
+            alignContent: 'flex-end',
+            alignItems: 'center',
+            top:'40%',
+            left:'48%',         
+          }
+          
+          } onPress={imagePickerCall}>
             <Entypo name="images" size={50} color="white" />
           </TouchableOpacity>    
         </View>
-      </Camera>
     </View>
   );
 }
@@ -135,6 +180,10 @@ const styles = StyleSheet.create({
       width: "100%",
       height: "100%",
       flex: 1,
+    },
+    cameradiv:{
+      flex:1,
+      width:'100%',
     },
   });
  
